91a6a79f9b31b370986c199df6dba52f01097651
Davi Pereira <davi.p.m94@gmail.com>
2026-02-05 17:00:27 +0900

    refactor: implement dashboard layout with collapsible sidebar, global store, and enhanced navigation components

--- app/(dashboard)/_components/dashboard-layout.tsx
+++ app/(dashboard)/_components/dashboard-layout.tsx
@@ -1,9 +1,142 @@
-import type { ReactNode } from 'react';
+'use client';
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { ChevronLeft, LogOut, Menu } from 'lucide-react';
+import { type ReactNode, useMemo, useState } from 'react';
+// import { useSignOut } from '@/app/(auth)/sign-in/_services/use-mutations';
+import { RouterGroup } from '@/app/(dashboard)/_components/router-group';
+import { Role } from '@/app/(dashboard)/_types/nav';
+import { ThemeToggle } from '@/components/theme-toggle';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
+import { useSession } from '@/lib/auth-client';
+import { ROUTE_GROUPS } from '@/lib/constants';
+import { cn } from '@/lib/utils';
+
+type Props = {
+  children: ReactNode;
+};
+
+/**
+ * Renders the dashboard layout component, which includes a collapsible sidebar,
+ * a top navigation bar, and a main content area. The layout adjusts based
+ * on the user's role and supports dynamic routing for the allowed role-based
+ * navigation groups.
+ *
+ * @param props The properties passed to the component.
+ * @param props.children The content to be displayed within the main layout area.
+ * @returns The rendered dashboard layout component.
+ */
+export function DashboardLayout({ children }: Props) {
+  const [open, setOpen] = useState(false);
+  // const { mutate: signOutMutation } = useSignOut();
+  const { data: session } = useSession();
+
+  const userRole = session?.user?.role === Role.ADMIN ? Role.ADMIN : Role.USER;
+
+  const filteredRouterGroup = useMemo(() => {
+    return ROUTE_GROUPS.filter((group) => group.allowedRoles.includes(userRole as Role.ADMIN));
+  }, [userRole]);
+
+  const handleSignOut = () => {
+    // signOutMutation();
+  };
+
   return (
-    <div>
-      <p>{children}</p>
+    <div className="flex">
+      <div className="bg-background fixed z-10 flex h-13 w-screen items-center justify-between border px-2">
+        <Collapsible.Root className="h-full" open={open} onOpenChange={setOpen}>
+          <Collapsible.Trigger asChild className="m-2">
+            <Button size="icon" variant="outline">
+              <Menu />
+            </Button>
+          </Collapsible.Trigger>
+        </Collapsible.Root>
+
+        <div className="flex">
+          <ThemeToggle />
+          {session && (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="flex h-9 items-center gap-2 px-2">
+                  <Avatar className="size-8">
+                    <AvatarFallback>{session.user?.name?.[0]}</AvatarFallback>
+                  </Avatar>
+                  <span className="hidden md:inline">{session.user?.name}</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <div className="flex items-center gap-3 px-2 py-1.5">
+                  <Avatar className="size-10">
+                    <AvatarFallback>{session.user?.name?.[0]}</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="text-sm font-medium">{session.user?.name}</p>
+                    <p className="text-muted-foreground text-sm">{session.user?.email}</p>
+                  </div>
+                </div>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onClick={handleSignOut}
+                  variant="destructive"
+                  className="hover:cursor-pointer"
+                >
+                  <LogOut className="size-4" /> Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </div>
+
+      <Collapsible.Root
+        className="fixed top-0 left-0 z-20 h-dvh"
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <Collapsible.Content forceMount>
+          <div
+            className={cn(
+              'bg-background fixed top-0 left-0 h-screen w-64 border p-4 transition-transform duration-300',
+              open ? 'translate-x-0' : '-translate-x-full',
+            )}
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold">
+                {userRole === Role.ADMIN ? 'Admin Dashboard' : 'Dashboard'}
+              </h1>
+              <Collapsible.Trigger asChild>
+                <Button size="icon" variant="outline">
+                  <ChevronLeft />
+                </Button>
+              </Collapsible.Trigger>
+            </div>
+
+            <Separator className="my-2" />
+
+            <div className="mt-4 flex flex-col">
+              {filteredRouterGroup.map((group) => (
+                <RouterGroup {...group} key={group.group} />
+              ))}
+            </div>
+          </div>
+        </Collapsible.Content>
+      </Collapsible.Root>
+
+      <main className={cn('mt-13 ml-0 flex-1 p-4 transition-all duration-300', open && 'ml-64')}>
+        {children}
+      </main>
     </div>
   );
 }
--- app/(dashboard)/_components/router-group.tsx
+++ app/(dashboard)/_components/router-group.tsx
@@ -1,17 +1,79 @@
 'use client';
 
-import { useState } from 'react';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { motion } from 'framer-motion';
+import { ChevronDown } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import type { RouteGroupType } from '@/app/(dashboard)/_types/nav';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
-type Props = {
-  name: string;
-};
+/**
+ * Renders a `RouterGroup` component that groups a set of navigational items under a collapsible
+ * section, with the ability to expand or collapse the group based on the current route.
+ *
+ * @param props - The props for the RouterGroup component.
+ * @param props.group - The name of the route group to be displayed.
+ * @param {Array} props.items - An array of route items, where each item represents a navigation link.
+ * Each item should include `href`, `icon`, and `label` properties.
+ * @return A collapsible component containing route items for navigation.
+ */
+export function RouterGroup({ group, items }: RouteGroupType) {
+  const pathname = usePathname();
+  const [open, setOpen] = useState(() => items.some((item) => pathname.startsWith(item.href)));
 
-export function RouterGroup({ name }: Props) {
-  const [item, setItem] = useState(null);
+  useEffect(() => {
+    setOpen(items.some((item) => pathname.startsWith(item.href)));
+  }, [items, pathname]);
 
   return (
-    <div>
-      <p></p>
-    </div>
+    <Collapsible.Root open={open} onOpenChange={setOpen}>
+      <Collapsible.Trigger asChild>
+        <Button
+          className="text-foreground/80 flex w-full justify-between font-normal"
+          variant="ghost"
+        >
+          {group}
+          <motion.div animate={{ rotate: open ? 180 : 0 }}>
+            <ChevronDown />
+          </motion.div>
+        </Button>
+      </Collapsible.Trigger>
+
+      <Collapsible.Content forceMount>
+        <motion.div
+          initial={{ opacity: 0, height: 0 }}
+          animate={{ height: open ? 'auto' : 0, opacity: open ? 1 : 0 }}
+          className={cn('flex flex-col gap-2', !open && 'pointer-events-none')}
+        >
+          {items.map((item) => {
+            const Icon = item.icon;
+            return (
+              <Button
+                key={item.href}
+                variant="link"
+                asChild
+                className="w-full justify-start font-normal"
+              >
+                <Link
+                  href={item.href}
+                  className={cn(
+                    'flex items-center rounded-md px-5 py-1 transition-all',
+                    pathname === item.href
+                      ? 'bg-foreground/10 hover:bg-foreground/5'
+                      : 'hover:bg-foreground/10',
+                  )}
+                >
+                  <Icon className="mr-2 size-3" />
+                  <span className="text-sm">{item.label}</span>
+                </Link>
+              </Button>
+            );
+          })}
+        </motion.div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
