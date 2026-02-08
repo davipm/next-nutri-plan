12f6d565219bd2764cd510b30a5130b4ad23b501
Davi Pereira <davi.p.m94@gmail.com>
2026-02-08 19:54:13 +0900

    feat: implement modern sidebar navigation with collapsible layout and breadcrumb navigation

---
+++ app/(dashboard)/_components/app-sidebar.tsx
@@ -0,0 +1,271 @@
+'use client';
+
+import {
+  BookOpen,
+  Bot,
+  ChevronRight,
+  ChevronsUpDown,
+  Command,
+  LogOut,
+  Settings2,
+  SquareTerminal,
+} from 'lucide-react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import type * as React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import { signOut, useSession } from '@/lib/auth-client';
+
+const data = {
+  navMain: [
+    {
+      title: 'Playground',
+      url: '#',
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: 'History',
+          url: '/admin',
+        },
+        {
+          title: 'Starred',
+          url: '#',
+        },
+        {
+          title: 'Settings',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Models',
+      url: '#',
+      icon: Bot,
+      items: [
+        {
+          title: 'Genesis',
+          url: '#',
+        },
+        {
+          title: 'Explorer',
+          url: '#',
+        },
+        {
+          title: 'Quantum',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Documentation',
+      url: '#',
+      icon: BookOpen,
+      items: [
+        {
+          title: 'Introduction',
+          url: '#',
+        },
+        {
+          title: 'Get Started',
+          url: '#',
+        },
+        {
+          title: 'Tutorials',
+          url: '#',
+        },
+        {
+          title: 'Changelog',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings2,
+      items: [
+        {
+          title: 'General',
+          url: '#',
+        },
+        {
+          title: 'Team',
+          url: '#',
+        },
+        {
+          title: 'Billing',
+          url: '#',
+        },
+        {
+          title: 'Limits',
+          url: '#',
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const router = useRouter();
+  const { isMobile } = useSidebar();
+  const { data: session } = useSession();
+
+  const handleSignOut = () => {
+    signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/sign-in');
+        },
+      },
+    });
+  };
+
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Nutri Plan</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Platform</SidebarGroupLabel>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={item.isActive}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild tooltip={item.title}>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                  {item.items?.length ? (
+                    <>
+                      <CollapsibleTrigger asChild>
+                        <SidebarMenuAction className="data-[state=open]:rotate-90">
+                          <ChevronRight />
+                          <span className="sr-only">Toggle</span>
+                        </SidebarMenuAction>
+                      </CollapsibleTrigger>
+                      <CollapsibleContent>
+                        <SidebarMenuSub>
+                          {item.items?.map((subItem) => (
+                            <SidebarMenuSubItem key={subItem.title}>
+                              <SidebarMenuSubButton asChild>
+                                <a href={subItem.url}>
+                                  <span>{subItem.title}</span>
+                                </a>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                          ))}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    </>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    {session?.user.image && (
+                      <AvatarImage src={session?.user.image} alt={session?.user.name} />
+                    )}
+                    <AvatarFallback className="rounded-lg">
+                      {session?.user.name.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">{session?.user.name}</span>
+                    <span className="truncate text-xs">{session?.user.email}</span>
+                  </div>
+                  <ChevronsUpDown className="ml-auto size-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                side={isMobile ? 'bottom' : 'right'}
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <Avatar className="h-8 w-8 rounded-lg">
+                      {session?.user.image && (
+                        <AvatarImage src={session?.user.image} alt={session?.user.name} />
+                      )}
+                      <AvatarFallback className="rounded-lg">
+                        {session?.user.name.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-medium">{session?.user.name}</span>
+                      <span className="truncate text-xs">{session?.user.email}</span>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuItem onClick={handleSignOut}>
+                  <LogOut />
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
--- app/(dashboard)/_components/dashboard-layout.tsx
+++ app/(dashboard)/_components/dashboard-layout.tsx
@@ -1,137 +1,61 @@
 'use client';
 
-import * as Collapsible from '@radix-ui/react-collapsible';
-import { ChevronLeft, LogOut, Menu } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { Activity, type ReactNode, useState } from 'react';
-import { RouterGroup } from '@/app/(dashboard)/_components/router-group';
-import { Role } from '@/app/(dashboard)/_types/nav';
-import { ThemeToggle } from '@/components/theme-toggle';
-import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
+import type { ReactNode } from 'react';
+import { AppSidebar } from '@/app/(dashboard)/_components/app-sidebar';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 import { Separator } from '@/components/ui/separator';
-import { signOut, useSession } from '@/lib/auth-client';
-import { ROUTE_GROUPS } from '@/lib/constants';
-import { cn } from '@/lib/utils';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 type Props = {
   children: ReactNode;
 };
 
 export function DashboardLayout({ children }: Props) {
-  const [open, setOpen] = useState(false);
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  const userRole = session?.user?.role === Role.ADMIN ? Role.ADMIN : Role.USER;
-  const filteredRouterGroup = ROUTE_GROUPS.filter((group) =>
-    group.allowedRoles.includes(userRole as Role.ADMIN),
-  );
-
-  const handleSignOut = () => {
-    signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push('/sign-in');
-        },
-      },
-    });
-  };
+  const pathname = usePathname();
+  const segments = pathname.split('/').filter(Boolean);
 
   return (
-    <div className="flex">
-      <div className="bg-background fixed z-10 flex h-13 w-screen items-center justify-between border px-2">
-        <Collapsible.Root className="h-full" open={open} onOpenChange={setOpen}>
-          <Collapsible.Trigger asChild className="m-2">
-            <Button size="icon" variant="outline">
-              <Menu />
-            </Button>
-          </Collapsible.Trigger>
-        </Collapsible.Root>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {segments.map((segment, index) => {
+                  const href = `/${segments.slice(0, index + 1).join('/')}`;
+                  const isLast = index === segments.length - 1;
+                  const label = segment.charAt(0).toUpperCase() + segment.slice(1);
 
-        <div className="flex">
-          <ThemeToggle />
-          <Activity mode={session ? 'visible' : 'hidden'}>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="flex h-9 items-center gap-2 px-2">
-                  <Avatar className="size-8">
-                    <AvatarFallback>{session?.user.name[0]}</AvatarFallback>
-                  </Avatar>
-                  <span className="hidden md:inline">{session?.user.name}</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <div className="flex items-center gap-3 px-2 py-1.5">
-                  <Avatar className="size-10">
-                    <AvatarFallback>{session?.user.name[0]}</AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <p className="text-sm font-medium">{session?.user.name}</p>
-                    <p className="text-muted-foreground text-sm">{session?.user.email}</p>
-                  </div>
-                </div>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  onClick={handleSignOut}
-                  variant="destructive"
-                  className="hover:cursor-pointer"
-                >
-                  <LogOut className="size-4" /> Log out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </Activity>
-        </div>
-      </div>
-
-      <Collapsible.Root
-        className="fixed top-0 left-0 z-20 h-dvh"
-        open={open}
-        onOpenChange={setOpen}
-      >
-        <Collapsible.Content forceMount>
-          <div
-            className={cn(
-              'bg-background fixed top-0 left-0 h-screen w-64 border p-4 transition-transform duration-300',
-              open ? 'translate-x-0' : '-translate-x-full',
-            )}
-          >
-            <div className="flex items-center justify-between">
-              <h1 className="font-semibold">
-                {userRole === Role.ADMIN ? 'Admin Dashboard' : 'Dashboard'}
-              </h1>
-              <Collapsible.Trigger asChild>
-                <Button size="icon" variant="outline">
-                  <ChevronLeft />
-                </Button>
-              </Collapsible.Trigger>
-            </div>
-
-            <Separator className="my-2" />
-
-            <div className="mt-4 flex flex-col">
-              {filteredRouterGroup.map((group) => (
-                <RouterGroup key={group.group} {...group} />
-              ))}
-            </div>
+                  return (
+                    <div key={href} className="flex items-center gap-2">
+                      <BreadcrumbSeparator />
+                      <BreadcrumbItem>
+                        {isLast ? (
+                          <BreadcrumbPage>{label}</BreadcrumbPage>
+                        ) : (
+                          <BreadcrumbLink href={href}>{label}</BreadcrumbLink>
+                        )}
+                      </BreadcrumbItem>
+                    </div>
+                  );
+                })}
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
-        </Collapsible.Content>
-      </Collapsible.Root>
-
-      <main className={cn('mt-13 ml-0 flex-1 p-4 transition-all duration-300', open && 'ml-64')}>
-        {children}
-      </main>
-    </div>
+        </header>
+        <div>{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
