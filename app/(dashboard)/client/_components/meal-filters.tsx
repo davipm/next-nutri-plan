81539e676874881241df6a232d453e1e3d906a97
Davi Pereira <davi.p.m94@gmail.com>
2026-03-15 21:51:52 +0900

    refactor: add meal card skeletons, UI enhancements for nutrition summary, and popover component integration

--- app/(dashboard)/client/_components/meal-filters.tsx
+++ app/(dashboard)/client/_components/meal-filters.tsx
@@ -1,14 +1,47 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { type SubmitHandler, useForm } from 'react-hook-form';
+import { Calendar, CalendarIcon } from 'lucide-react';
+import { useState } from 'react';
+import { Controller, type SubmitHandler, useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
-import { FieldGroup } from '@/components/ui/field';
+import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from '@/components/ui/input-group';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { type MealFiltersInput, mealFiltersSchema } from '@/server/modules/meal/meal.schema';
-import { useMealFilters } from '@/store/use-meal-store';
+import { useMealFilterActions, useMealFilters } from '@/store/use-meal-store';
+
+function formatDate(date: Date | undefined) {
+  if (!date) {
+    return '';
+  }
+  return date.toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  });
+}
+
+function isValidDate(date: Date | undefined) {
+  if (!date) {
+    return false;
+  }
+  return !isNaN(date.getTime());
+}
 
 export function MealFilters() {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<Date | undefined>(new Date('2025-06-01'));
+  const [month, setMonth] = useState<Date | undefined>(date);
+  const [value, setValue] = useState(formatDate(date));
+
   const mealFilters = useMealFilters();
+  const { setFilters } = useMealFilterActions();
 
   const form = useForm({
     defaultValues: mealFilters,
@@ -16,28 +49,75 @@ export function MealFilters() {
   });
 
   const onSubmit: SubmitHandler<MealFiltersInput> = (data) => {
-    console.log(data);
+    setFilters(data);
   };
 
   return (
     <form className="mb-4 flex items-center gap-3" onSubmit={form.handleSubmit(onSubmit)}>
       <FieldGroup className="col-span-1 grid">
-        {/*<Controller*/}
-        {/*  control={form.control}*/}
-        {/*  name="dateTime"*/}
-        {/*  render={({ field, fieldState }) => (*/}
-        {/*    <Field data-invalid={fieldState.invalid}>*/}
-        {/*      <Input*/}
-        {/*        {...field}*/}
-        {/*        aria-invalid={fieldState.invalid}*/}
-        {/*        id={field.name}*/}
-        {/*        placeholder="Enter food name"*/}
-        {/*        type="text"*/}
-        {/*      />*/}
-        {/*      {fieldState.invalid && <FieldError errors={[fieldState.error]} />}*/}
-        {/*    </Field>*/}
-        {/*  )}*/}
-        {/*/>*/}
+        <Controller
+          control={form.control}
+          name="dateTime"
+          render={({ field, fieldState }) => (
+            <Field className="mx-auto w-48">
+              <FieldLabel htmlFor="date-required">Subscription Date</FieldLabel>
+              <InputGroup>
+                <InputGroupInput
+                  id="date-required"
+                  onChange={(e) => {
+                    const date = new Date(e.target.value);
+                    setValue(e.target.value);
+                    if (isValidDate(date)) {
+                      setDate(date);
+                      setMonth(date);
+                    }
+                  }}
+                  onKeyDown={(e) => {
+                    if (e.key === 'ArrowDown') {
+                      e.preventDefault();
+                      setOpen(true);
+                    }
+                  }}
+                  placeholder="June 01, 2025"
+                  value={value}
+                />
+                <InputGroupAddon align="inline-end">
+                  <Popover onOpenChange={setOpen} open={open}>
+                    <PopoverTrigger asChild>
+                      <InputGroupButton
+                        aria-label="Select date"
+                        id="date-picker"
+                        size="icon-xs"
+                        variant="ghost"
+                      >
+                        <CalendarIcon />
+                        <span className="sr-only">Select date</span>
+                      </InputGroupButton>
+                    </PopoverTrigger>
+                    <PopoverContent
+                      align="end"
+                      alignOffset={-8}
+                      className="w-auto overflow-hidden p-0"
+                      sideOffset={10}
+                    >
+                      <Calendar
+                        mode="single"
+                        month={month}
+                        onMonthChange={setMonth}
+                        onSelect={(date) => {
+                          setDate(date);
+                          setValue(formatDate(date));
+                          setOpen(false);
+                        }}
+                        selected={date}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </InputGroupAddon>
+              </InputGroup>
+            </Field>
+          )}
+        />
       </FieldGroup>
 
       <Button size="sm" type="submit">
