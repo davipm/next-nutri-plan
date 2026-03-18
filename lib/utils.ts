c601cd13145baa9f5edfbbc3b0877aa9623f7267
eli <cengha720@hotmail.com>
2026-03-18 18:20:24 +0900

    refactor(utils): remove unused `toStringSafe` and `toNumberSafe` functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,22 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const toStringSafe = (value: unknown): string => {
-  return value === null ? '' : String(value);
-};
-
-export const toNumberSafe = (value: unknown): number => {
-  if (value === null) {
-    return 0;
-  }
-  if (typeof value === 'number') {
-    return value;
-  }
-
-  const parsedValue = Number(value);
-  return Number.isNaN(parsedValue) ? 0 : parsedValue;
-};
-
 export const routes = {
   home: '/',
   signIn: '/sign-in',
